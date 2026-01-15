1e65fe793dc4db0d8ce68e643168e8b4fa35b081
Wolf Zimmermann <wolf.zimmermann@educaiton.vic.gov.au>
2026-01-15 13:50:01 +1100

    add recipe type to schema

--- server/src/models/recipe.js
+++ server/src/models/recipe.js
@@ -6,6 +6,10 @@ const recipeSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  type: {
+    type: String,
+    required: true,
+  },
   description: {
     type: String,
   },
