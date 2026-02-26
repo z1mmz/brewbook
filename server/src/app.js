0759896b4f80af9125cdde9464882ae154c82a31
Wolf ZImmermann <wolfz@live.com>
2026-02-27 09:28:25 +1100

    setup build for render

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const db = require('./utils/db')
+const express = require("express");
+const db = require("./utils/db");
+const path = require("path");
 
-const middleware = require('./utils/middleware')
-const recipesRouter = require('./controllers/recipes')
-const usersRouter = require('./controllers/users')
-const reviewsRouter = require('./controllers/reviews')
-const loginRouter = require('./controllers/login')
-const app = express()
+const middleware = require("./utils/middleware");
+const recipesRouter = require("./controllers/recipes");
+const usersRouter = require("./controllers/users");
+const reviewsRouter = require("./controllers/reviews");
+const loginRouter = require("./controllers/login");
+const app = express();
 
-app.use(express.json())
-app.use(middleware.requestLogger)
-app.use(middleware.tokenExtractor)
-app.use('/api/recipes', recipesRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/reviews', reviewsRouter)
-app.use('/api/login', loginRouter)
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-module.exports = app
+app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
+app.use("/api/recipes", recipesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/reviews", reviewsRouter);
+app.use("/api/login", loginRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+module.exports = app;
--- web/vite.config.js
+++ web/vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "../server/src/dist",
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         changeOrigin: true,
       },
     },
   },
-})
+});
